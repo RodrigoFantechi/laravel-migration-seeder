32a3340cdf99354fb4a703ea5df8bb3d8cd313c7
RodrigoFantechi <rodrifante595@libero.it>
2022-12-20 15:45:20 +0100

    add controller and migration

---
+++ database/migrations/2022_12_20_134159_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->string('company', 50);
+            $table->string('departure_station', 80);
+            $table->string('arrival_station', 80);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->tinyInteger('train_code');
+            $table->tinyInteger('number_of_carriages')->unsigned()->nullable();
+            $table->boolean('in_time')->default(true);
+            $table->boolean('in_cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
